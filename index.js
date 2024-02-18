905f35b815f267f236d0fd6c18a6cbfe37c6acd1
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-02-18 19:37:40 +0000

    Set the with and height of the container and divs to fit 16x16,made a mous over function for the divs to make it look like im painting on the div

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const container = document.createElement("div");
 
-container.innerText = "hello";
-
 container.id = "container"
 
 document.body.appendChild(container);
 
+
 for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
-     var block = document.createElement('div');
+    let block = document.createElement('div');
     block.className = "block";
 
-    block.innerText = "HELLO    "
+    document.getElementById('container').appendChild(block);    
 
-    document.getElementById('container').appendChild(block);
-    }
+    block.addEventListener('mouseover', function() {
+        block.style.backgroundColor = 'black'
+    })
+}
 }
 
-const divCount = document.querySelectorAll('div').length;
-console.log("Number of divs:", divCount);
+
+
+
+
+
 
