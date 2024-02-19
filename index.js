c5d191ce8d930c5d232d8be5dcb98d3421fbf35e
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-02-19 18:14:07 +0000

    Made the elements more responsive using JS, also deleted fixed sizes of the container,

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const container = document.createElement("div");
-
-container.id = "container"
-
-document.body.appendChild(container);
+const container = document.getElementById('container');
+const btn = document.getElementById('resetBtn')
 
 
-for (i = 0; i < 16; i++) {
-    for (j = 0; j < 16; j++) {
-    let block = document.createElement('div');
-    block.className = "block";
+function createGrid(size) {
+     container.innerText = "";
+     container.style.width = `${size * 3}rem`;
+     for (let i = 0; i < size * size; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        container.appendChild(block);
 
-    document.getElementById('container').appendChild(block);    
+        block.addEventListener('mouseover', function() {
+            block.style.backgroundColor = 'black';
+        })
+        
+     }
 
-    block.addEventListener('mouseover', function() {
-        block.style.backgroundColor = 'black'
-    })
 }
-}
 
+btn.addEventListener('click', () => {
+    let newSize = prompt('How many block would you like inside your grid?')
+    newSize = parseInt(newSize);
 
+    if(!isNaN(newSize) && newSize > 0 && newSize < 100) {
+        createGrid(newSize);
+    } else {
+        alert('Put in a valid number that is more then 0 and less then 100');
+    }
+})
 
 
 
